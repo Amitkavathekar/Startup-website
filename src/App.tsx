15f9fb196df3089105c706eed5b4529883c86f0e
Amitkavathekar <amitkavathekar123@gmail.com>
2026-07-06 17:26:03 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Routes from "./routes/AppRoutes";
+import Navbar from "./Components/navbar/navbar";
+
+const App = () => {
+  return (
+    <div className="bg-red-200">
+      <Navbar />
+      <Routes />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/routes/AppRoutes.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+const Home = () => <div>Home</div>;
+
+const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<div>About</div>} />
+      <Route path="/contact" element={<div>Contact</div>} />
+    </Routes>
+  );
+};
+
+export default AppRoutes;
